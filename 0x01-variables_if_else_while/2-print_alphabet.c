dc914b0e6e52303787b1a8796df429f9817059cc
MissTipo <dorine.a.tipo@gmail.com>
2022-06-03 10:28:47 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print the alphabet in lower case
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+for (c = 0; c < 26; ++c)
+putchar('a' + c);
+putchar('\n');
+return (0);
+}
