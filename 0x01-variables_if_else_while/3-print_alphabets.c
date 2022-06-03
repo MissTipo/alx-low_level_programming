fed821483268784fd6cb1bed9d1ba2761e0d29c2
MissTipo <dorine.a.tipo@gmail.com>
2022-06-03 10:42:11 -0700

    Alphabet c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print the alphabet in lower and uppercase
+ */
+int main(void)
+{
+int c;
+for (c = 0; c < 26; ++c)
+putchar('a' + c);
+for (c = 0; c < 26; ++c)
+putchar('A' + c);
+putchar('\n');
+return (0);
+}
