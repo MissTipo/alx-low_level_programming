e2f6362e36775bf0df02fad3f410c9a7b2623aee
MissTipo <dorine.a.tipo@gmail.com>
2022-06-03 11:05:29 -0700

    Print five

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Prints all single digits of base 10
+ * Return: 0
+ */
+main int(void)
+{
+char i;
+for (i = '0'; i <= '9'; ++i)
+putchar(i);
+putchar('\n');
+return (0);
+}
