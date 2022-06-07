cb765e6b5d707a413f4223b64f1b4bf7ef22ef0c
MissTipo <dorine.a.tipo@gmail.com>
2022-06-07 15:23:42 -0700

    natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,12 +3,13 @@
 /**
  * print_to_98 - Print all natural numbers from n to 98
  * @n: int type number
+ * Return: void returns nothing
  */
 void print_to_98(int n)
 {
 if (n < 98)
 {
-printf(n = n; n < 98; n++)
+printf(n = n); n < 98; n++)
 printf("%d, ", n);
 printf("%d\n", 98);
 }
