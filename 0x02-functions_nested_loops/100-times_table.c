b1cc98ddc6068b6db224bde388b3b1eef024af39
MissTipo <dorine.a.tipo@gmail.com>
2022-06-08 08:15:26 -0700

    print timetable

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+void print_times_table(int n)
+{
+	return (0);
+}
