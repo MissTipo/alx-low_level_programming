a0d66c76a71d27fd38f598c0567d864acaaa60d1
MissTipo <dorine.a.tipo@gmail.com>
2022-06-09 09:26:33 -0700

    numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - Print the numbers since 0 up to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i = 48;
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
