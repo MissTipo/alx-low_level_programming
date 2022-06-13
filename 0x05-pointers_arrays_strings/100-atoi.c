7eb400e5815ce5ae073777548c4780070c575c75
MissTipo <dorine.a.tipo@gmail.com>
2022-06-13 06:41:57 -0700

    great leaders

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,6 +9,7 @@ int _atoi(char *s)
 {
 	int sign = 1;
 	unsigned int num = 0;
+
 	do {
 		if (*s == '-')
 			sign *= -1;
@@ -17,5 +18,6 @@ int _atoi(char *s)
 		else if (num > 0)
 			break;
 	} while (*s++);
+
        return (num * sign);
 }	
