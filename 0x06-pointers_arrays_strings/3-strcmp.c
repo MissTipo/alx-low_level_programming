b62a88928ee577bc860dee32ad32cbc4744afa41
MissTipo <dorine.a.tipo@gmail.com>
2022-06-17 07:12:14 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,13 +4,13 @@
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: string to compare by
- * Return : the difference between s1 and s2
+ * Return : always 0(Success)
  */
-int _strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2);
 {
-	int i;
+	int i = 0;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++)
 		;
-	return (s1[i] - s2[i]);
+	return (0);
 }
