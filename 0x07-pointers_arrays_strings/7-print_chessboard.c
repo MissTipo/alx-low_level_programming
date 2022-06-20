873b876fd59ede3ab79a90fbb45859ecd1f55374
MissTipo <dorine.a.tipo@gmail.com>
2022-06-20 06:20:26 -0700

    chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void print_chessboard(char (*a)[8])
+{
+	int p, q;
+
+	for (p = 0; p < 8; p++)
+	{
+		for (q = 0; q < 8; q++)
+		{
+			_putchar(a[p][q]);
+		}
+		_putchar('\n');
+	}
+}
