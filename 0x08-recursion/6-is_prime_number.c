8e3069d5d3672eac1bd02d6f1918fab22ac2ca61
MissTipo <dorine.a.tipo@gmail.com>
2022-06-22 07:32:20 -0700

    returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int check_prime(int n, int i);
+
 /**
  * is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0.
  * @n: the number to be checked
