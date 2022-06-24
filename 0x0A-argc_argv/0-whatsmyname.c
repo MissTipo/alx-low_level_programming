7614ff15555118d3ef5978e97ae51758dd53c928
MissTipo <dorine.a.tipo@gmail.com>
2022-06-24 08:41:50 -0700

    Write a program that prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib>
 
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: argument count for main
+ * @argv: vector to the arguments
+ */
 int main(int argc, char **argv)
 {
 	if (argc >= 1)
