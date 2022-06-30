f2e3aa626cf0e4820cf5fc5f821e50853816ad3b
MissTipo <dorine.a.tipo@gmail.com>
2022-06-30 07:43:19 -0700

    header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -5,7 +8,9 @@ char *_memset(char *s, char b, unsigned int n);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *_memcpy(char *dest, char *src, unsigned int n);
-int main(int argc, char *argv[]);
+int _putchar(char c);
 int _isnumber(char *str);
 void _error(void);
 int _strlen(char *s);
+
+#endif
