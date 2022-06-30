a2e05e693588f772832958aac378911d1ac0c959
MissTipo <dorine.a.tipo@gmail.com>
2022-06-30 07:48:21 -0700

    header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,7 +8,6 @@ char *_memset(char *s, char b, unsigned int n);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *_memcpy(char *dest, char *src, unsigned int n);
-int _putchar(char c);
 int _isnumber(char *str);
 void _error(void);
 int _strlen(char *s);
