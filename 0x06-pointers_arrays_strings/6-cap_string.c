68d927282d5a9efddb9e8351249a097bb3a8b90f
MissTipo <dorine.a.tipo@gmail.com>
2022-07-26 03:22:30 -0700

    Write a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,34 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
- * @s: string to capitalize
- * Return: Returns pointer to capitalized string
+ * cap_string - capitalize the words in a strings
+ * @s: the string to capitalize
+ *
+ * Description: The following characters are considered word separators:
+ * "<space> <tab> <newline> , ; . ! ? \" ( ) { }"
+ *
+ * Return: pointer to the capitalized string
  */
-char *cap_string(char *)
+char *cap_string(char *s)
 {
-	int i = 0; j;
-	char sep[] = " \t\n,;.!?\"(){}";
+	char *ws = " \t\n,;.!?\"(){}";
+	int i;
+	int j;
 
-	i = 1;
-	if (s[0] >= 'a' && s[0] <= 'z')
-		s[0] -= ('a' - 'A');
-	while (s[i] != '\0')
+	for (i = 0, j = 0; s[i]; ++i)
 	{
-		for (j = 0; sep[j] != '\0'; j++)
-			if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z'))
+		if ('a' <= s[i] && s[i] <= 'z')
+		{
+			if (i)
+			{
+				for (j = 0; ws[j] && ws[j] != s[i - 1]; ++j)
+					;
+			}
+
+			if (ws[j])
 				s[i] -= ('a' - 'A');
-		i++;
+		}
 	}
+
+	return (s);
 }
